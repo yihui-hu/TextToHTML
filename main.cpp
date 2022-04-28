973b0cb3ec823e5ee61fed45edd0de668cd32018
Yihui <yyihui.hu@gmail.com>
2022-04-28 09:39:06 -0400

    program now also encapsulates HTML snippet in boilerplate (specific to my website)

--- main.cpp
+++ main.cpp
@@ -8,6 +8,9 @@
 
 using namespace std;
 
+void front_boilerplate(string noteTitle, ostream &outputFile);
+void back_boilerplate(ostream &outputFile);
+
 int main(int argc, char *argv[])
 {
     if (argc != 3)
@@ -24,16 +27,50 @@ int main(int argc, char *argv[])
 
     string line;
 
+    inputFile >> line;
+
+    front_boilerplate(line, outputFile);
+
     while (getline(inputFile, line))
     {
         if (line == "")
             continue;
         else
         {
-            outputFile << "<p>" << endl;
-            outputFile << line << endl;
-            outputFile << "</p>" << endl
+            outputFile << "            <p>" << endl;
+            outputFile << "            " << line << endl;
+            outputFile << "            </p>" << endl
                        << endl;
         }
     }
+
+    back_boilerplate(outputFile);
+}
+
+void front_boilerplate(string noteTitle, ostream &outputFile)
+{
+    outputFile << "<!DOCTYPE html>" << endl;
+    outputFile << "<html lang=\"en\" dir=\"ltr\">" << endl;
+    outputFile << "    <head>" << endl;
+    outputFile << "        <meta charset=\"utf-8\">" << endl;
+    outputFile << "        <title>" << noteTitle << "</title>" << endl;
+    outputFile << "        <meta name=\"viewport\" content=\"width=device-width,initial-scale=1.0\">" << endl;
+    outputFile << "        <link rel=\"stylesheet\" href=\"../../styles.css\">" << endl;
+    outputFile << "    </head>" << endl;
+    outputFile << "    <body>" << endl;
+    outputFile << "        <div class=\"container\">" << endl;
+    outputFile << "            <div class=\"header\">" << endl;
+    outputFile << "                " << noteTitle << endl;
+    outputFile << "            </div>" << endl
+               << endl;
+}
+
+void back_boilerplate(ostream &outputFile)
+{
+    outputFile << "            <p></p>" << endl;
+    outputFile << "            <a class = \"main\" href = \"../notes.html\">⚛&#xFE0E;return to notes</a>" << endl;
+    outputFile << "            <a class = \"main\" href = \"../../index.html\">⁖ return to main</a>" << endl;
+    outputFile << "        </div>" << endl;
+    outputFile << "    </body>" << endl;
+    outputFile << "</html>" << endl;
 }
