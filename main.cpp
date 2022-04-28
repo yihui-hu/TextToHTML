9a2b68622bb1933c474bf0adc0cf348ed92f0aba
Yihui <yyihui.hu@gmail.com>
2022-04-28 00:56:03 -0400

    main program of TextToHTML

--- main.cpp
+++ main.cpp
@@ -2,10 +2,38 @@
 // 28/04/22 Hu Yihui
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
-    cout << "hello world!" << endl;
+    if (argc != 3)
+    {
+        cerr << "Usage: ./TextToHTML inputFile outputFile" << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    std::ifstream inputFile;
+    inputFile.open(argv[1]);
+
+    std::ofstream outputFile;
+    outputFile.open(argv[2]);
+
+    string line;
+
+    while (getline(inputFile, line))
+    {
+        if (line == "")
+            continue;
+        else
+        {
+            outputFile << "<p>" << endl;
+            outputFile << line << endl;
+            outputFile << "</p>" << endl
+                       << endl;
+        }
+    }
 }
